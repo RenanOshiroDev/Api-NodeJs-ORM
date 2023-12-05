c493a7f86775f63b381347d13eb326a3cf69ea32
RenanOshiroDev <renanoshiro.dev@gmail.com>
2023-12-05 16:44:02 -0300

    Api NodeJs Sequelize e MySql

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const routes = require("./routes")
+const routes = require('./routes')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 routes(app)
 
-app.listen(port, ()=> console.log(`Servidor escutando na porta ${port}`));
+app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
-module.exports = app;
+module.exports = app
